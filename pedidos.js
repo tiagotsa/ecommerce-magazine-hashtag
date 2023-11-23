407385c2f39ae1bf426a28f4591f46b9fee07554
tiagotsa <95516711+tiagotsa@users.noreply.github.com>
2023-11-23 14:31:50 -0300

    Update pedidos.js

--- pedidos.js
+++ pedidos.js
@@ -1,4 +1,4 @@
-import { lerLocalStorage, desenharProdutoNoCarrinhoSimples, } from "./src/utilidades";
+import { lerLocalStorage, desenharProdutoNoCarrinhoSimples, } from "./src/utilidades.js";
 
 function criarPedidoHistorico(pedidoComData) {
     const elementoPedido = `<p class='text-xl text-bold my-4' >${new Date(pedidoComData.dataPedido).toLocaleDateString("pt-BR", { hour: "2-digit", minute: "2-digit",})}</p>
